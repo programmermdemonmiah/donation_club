db49cf775a74639ecb936b96ecf55a89af93a3ac
Hasanur Rahman Kabir <hasanurrahmankabir@gmail.com>
2026-09-15 22:43:37 +0600

    updated user dashboard page

--- resources/js/pages/dashboard/Index.tsx
+++ resources/js/pages/dashboard/Index.tsx
@@ -78,7 +78,7 @@ export default function Dashboard() {
                     icon="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3" />
                 <StatCard label="Total Donate" value={formatMoney(stats.total_deposit)} accent="blue"
                     icon="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33" />
-                <StatCard label="Total Support" value={formatMoney(stats.total_return)} accent="emerald"
+                <StatCard label="Total Income" value={formatMoney(stats.total_return)} accent="emerald"
                     icon="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 <StatCard label="Total Generation" value={formatMoney(stats.total_commission)} accent="emerald"
                     icon="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
